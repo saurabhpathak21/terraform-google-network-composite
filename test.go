1912a274da36a3bdbe404c96f40e791ba48c776b
Saurabh <saurabhpathaks21@gmail.com>
2023-04-13 14:33:18 +0100

    update the testcase

--- test.go
+++ test.go
@@ -1,130 +1,73 @@
 package test
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-	"testing"
-	"time"
+    "fmt"
+    "testing"
 
-	"github.com/gruntwork-io/terratest/modules/gcp"
-	"github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/gruntwork-io/terratest/modules/gcp"
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
 )
 
-const (
-	expectedProjectId    = "my-project-id"
-	expectedNetwork      = "my-network"
-	expectedSubnetName   = "my-subnet"
-	expectedSubnetCidr   = "10.0.0.0/24"
-	expectedRouterName   = "my-router"
-	expectedRouterRegion = "us-central1"
-	expectedVpnName      = "my-vpn"
-	terraformCodeDir = "../"
-)
+func TestProjectCreation(t *testing.T) {
+    expectedProjectName := "acceleration-test"
+    expectedRegion := "europe-west2"
+    expectedNetworkName := "test"
+    expectedFirewallRules := []interface{}{
+        map[string]interface{}{
+            "name":      "allow-http",
+            "direction": "ingress",
+            "allow": []interface{}{
+                map[string]interface{}{
+                    "protocol": "tcp",
+                    "ports":    []interface{}{"80"},
+                },
+            },
+            "priority": 1000,
+        },
+    }
 
-var terraformOptions = &terraform.Options{
-	TerraformDir: terraformCodeDir,
-	Vars: map[string]interface{}{
-		"type":          "acceleration",
-		"organization_id": "my-org-id",
-		"folder_id":        "my-folder-id",
-		"billing_account":  "my-billing-account",
-		"default_network_tier": "PREMIUM",
-		"firewall_rules": []map[string]interface{}{
-			{
-				"name":       "ssh",
-				"direction":  "INGRESS",
-				"ranges":     []string{"0.0.0.0/0"},
-				"target_tags":[]string{"web"},
-				"allow": []map[string]string{
-					{
-						"protocol": "tcp",
-						"ports":    "22",
-					},
-				},
-			},
-		},
-		"subnets": []map[string]interface{}{
-			{
-				"name":  expectedSubnetName,
-				"cidr":  expectedSubnetCidr,
-				"region": "us-central1",
-			},
-		},
-		"secondary_ranges": []map[string]interface{}{
-			{
-				"subnet_name":    expectedSubnetName,
-				"range_name":     "my-secondary-range",
-				"ip_cidr_range":  "172.16.0.0/24",
-			},
-		},
-		"routes": []map[string]interface{}{
-			{
-				"name":                      "my-route",
-				"network":                   expectedNetwork,
-				"dest_range":                "10.0.1.0/24",
-				"next_hop_network_interface": expectedSubnetName,
-			},
-		},
-		"router_name": expectedRouterName,
-		"target_network_self_link": "my-target-network",
-	},
-}
+    terraformOptions := &terraform.Options{
+        TerraformDir: ".",
+        Vars: map[string]interface{}{
+            "type":                  "test",
+            "organization_id":       "32792173",
+            "folder_id":             "312900238",
+            "billing_account":       "97320230",
+            "default_network_tier":  "Premium",
+            "firewall_rules":        expectedFirewallRules,
+        },
+    }
 
-func TestTerratest(t *testing.T) {
+    defer terraform.Destroy(t, terraformOptions)
+    terraform.InitAndApply(t, terraformOptions)
 
-	// set the environment variables
-	os.Setenv("GOOGLE_PROJECT", expectedProjectId)
+    projectId := terraform.Output(t, terraformOptions, "project_id")
+    networkUrl := fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/global/networks/%s", projectId, expectedNetworkName)
+    firewallUrl := fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/global/firewalls/allow-http", projectId)
 
-	// deploy the infrastructure code
-	terraform.InitAndApply(t, terraformOptions)
+    // Check the project was created with the expected name and region
+    actualProjectName := gcp.GetProjectName(t, projectId)
+    actualRegion := gcp.GetProjectRegion(t, projectId)
+    assert.Equal(t, expectedProjectName, actualProjectName)
+    assert.Equal(t, expectedRegion, actualRegion)
 
-	// test that the project was created
-	project := gcp.GetProject(t, expectedProjectId)
-	if project == nil {
-		t.Fatalf("Project %q was not created", expectedProjectId)
-	}
+    // Check the network was created with the expected name
+    actualNetworkName := gcp.GetNetworkName(t, networkUrl)
+    assert.Equal(t, expectedNetworkName, actualNetworkName)
 
-	// test that the network was created
-	network := gcp.GetNetwork(t, expectedProjectId, expectedNetwork)
-	if network == nil {
-		t.Fatalf("Network %q was not created", expectedNetwork)
-	}
+    // Check the firewall rule was created with the expected properties
+    actualFirewallRules := gcp.GetFirewallRules(t, projectId)
+    assert.Len(t, actualFirewallRules, 1)
+    actualFirewallRule := actualFirewallRules[0]
+    assert.Equal(t, expectedFirewallRules[0].(map[string]interface{})["name"], actualFirewallRule.Name)
+    assert.Equal(t, expectedFirewallRules[0].(map[string]interface{})["direction"], actualFirewallRule.Direction)
+    assert.Equal(t, expectedFirewallRules[0].(map[string]interface{})["priority"], actualFirewallRule.Priority)
+    assert.Equal(t, expectedFirewallRules[0].(map[string]interface{})["allow"], actualFirewallRule.Allow)
+    assert.Equal(t, expectedFirewallRules[0].(map[string]interface{})["deny"], actualFirewallRule.Deny)
+    assert.Equal(t, expectedFirewallRules[0].(map[string]interface{})["log_config"], actualFirewallRule.LogConfig)
 
-	// test that the subnet was created
-	subnet := gcp.GetSubnetwork(t, expectedProjectId, expectedSubnetName)
-	if subnet == nil {
-		t.Fatalf("Subnet %q was not created", expectedSubnetName)
-	}
-
-	// test that the IP range matches
-	if subnet.IpCidrRange != expectedSubnetCidr {
-		t.Fatalf("Subnet IP range %q does not match expected %q", subnet.IpCidrRange, expectedSubnetCidr)
-	}
-
-	// test that the router was created
-	router := gcp.GetRouter(t, expectedProjectId, expectedRouterRegion, expectedRouterName)
-	if router == nil {
-		t.Fatalf("Router %q was not created", expectedRouterName)
-	}
-
-	// test that the VPN was created
-	vpn := gcp.GetVPN(t, fmt.Sprintf("projects/%s/regions/%s/vpnGateways/%s", expectedProjectId, expectedRouterRegion, expectedVpnName))
-	if vpn == nil {
-		t.Fatalf("VPN %q was not created", expectedVpnName)
-	}
-
-	// wait for the VPN to be fully created
-	maxRetries := 5
-	timeBetweenRetries := 10 * time.Second
-	retryableErrors := []string{
-		"operation is not ready",
-	}
-	err := gcp.WaitForVPNConnectionStatusE(t, vpn.Id, maxRetries, timeBetweenRetries, retryableErrors)
-	if err != nil {
-		t.Fatalf("Failed waiting for VPN connection status: %s", err)
-	}
-	// clean up infrastructure resources after the test case ends
-	defer terraform.Destroy(t, terraformOptions)
+    // Check the firewall rule applies to the network
+    actualFirewallRuleNetworks := gcp.GetFirewallRuleNetworks(t, firewallUrl)
+    assert.Contains(t, actualFirewallRuleNetworks, expectedNetworkName)
 }
