3fabe67dbc269c3b947113915eec085db107fa36
Saurabh <saurabhpathaks21@gmail.com>
2023-04-17 13:00:24 +0100

    Update and rename main.go to project.go

--- project.go
+++ project.go
@@ -3,11 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-    compute "cloud.google.com/go/compute/apiv1"
     "context"
-    "google.golang.org/api/iterator"
-    computepb "google.golang.org/genproto/googleapis/cloud/compute/v1"
-	
     "google.golang.org/api/cloudresourcemanager/v1"
 	"google.golang.org/api/option"
 )
@@ -35,28 +31,3 @@ func main() {
 
 }
 
-func network() {
-    ctx := context.Background()
-    c, err := compute.NewNetworksRESTClient(ctx)
-    if err != nil {
-            // TODO: Handle error.
-    }
-    defer c.Close()
-
-    req := &computepb.ListNetworksRequest{
-            // TODO: Fill request struct fields.
-            // See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/compute/v1#ListNetworksRequest.
-    }
-    it := c.List(ctx, req)
-    for {
-            resp, err := it.Next()
-            if err == iterator.Done {
-                    break
-            }
-            if err != nil {
-                    // TODO: Handle error.
-            }
-            // TODO: Use resp.
-            _ = resp
-    }
-}
